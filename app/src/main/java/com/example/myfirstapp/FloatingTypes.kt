ebfbd488b929a514a9b78e8305d17c745fdf18ba
Nilesh Kumar D <nileshkotle123@gmail.com>
2024-01-04 01:50:59 +0530

    variable types, floating type, unsigned integers

---
+++ app/src/main/java/com/example/myfirstapp/FloatingTypes.kt
@@ -0,0 +1,13 @@
+package com.example.myfirstapp
+
+fun main(){
+    var circleRatio = 3.14f;
+    var withoutFloat = 3.14;
+    circleRatio = 3.1415926535F
+    withoutFloat = 3.1415926535
+    println(circleRatio) //3.1415927
+    println(withoutFloat) //3.1415926535
+
+    var age:UShort = 35u
+    println(age)//UShort is similar to Short but only takes postitive range -- Unsinged integers
+}
---
+++ app/src/main/java/com/example/myfirstapp/VariableTypes.kt
@@ -0,0 +1,10 @@
+package com.example.myfirstapp
+
+
+//int  default
+//short var k:Short  = 3
+//long var k:Long = 9
+
+
+//float- has restriction on the number of digits being used.
+//double
