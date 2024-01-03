40fef4fb3715a3f35c1bb521ced9f4f30891d4d0
Nilesh Kumar D <nileshkotle123@gmail.com>
2024-01-04 01:30:44 +0530

    differnce between var and val

---
+++ app/src/main/java/com/example/myfirstapp/Basics.kt
@@ -0,0 +1,11 @@
+package com.example.myfirstapp
+
+fun main(){
+    println("hellow world")
+
+//    val data = 30 -- Immutable
+    var key = 40 // Mutable
+    println(key)
+    key = 90
+    println(key)
+}
